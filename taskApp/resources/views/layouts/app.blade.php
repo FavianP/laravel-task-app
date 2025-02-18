bef0b58d5395e9ed171eaee0c32a3953fb4edbf0
Favian Perez <142631192+FavianP@users.noreply.github.com>
2025-02-18 21:29:28 +0000

    first commit

---
+++ taskApp/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+@vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100">
+
+<nav class = "bg-white shadow p-4">
+
+   <h1 class = "text-xl font-bold text-blue-50"> My Task App </h1>
+
+</nav>
+
+<main class="p-4">
+    @yield('content')
+</main>
+@vite('resources/js/app.js')
+
+</body>
+</html>
